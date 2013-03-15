104b87d5d7f22b9f6c6a0eebcd55df12cd8c4046
Matt Brewer <matt.brewer@me.com>
2013-03-15 10:06:51 -0400

    Authorization#reset_with_role! added

--- lib/can_i/version.rb
+++ lib/can_i/version.rb
@@ -1,3 +1,3 @@
 module CanI
-  Version = "0.1.1"
+  Version = "0.1.2"
 end
--- spec/authorization_roles_spec.rb
+++ spec/authorization_roles_spec.rb
@@ -35,4 +35,31 @@ describe "auth roles" do
 
   end
 
+  describe "resetting a role" do
+
+    before do
+      @auth = CanI::Authorization.new :admin
+    end
+
+    it "should be an admin role" do
+      @auth.auth_role.should.be.instance_of AdminRole
+    end
+
+    it "should let me reset back to the default role" do
+      @auth.reset_with_role! :authorization
+      @auth.auth_role.should.be.instance_of CanI::AuthorizationRole
+    end
+
+    it "should not have any allowed actions" do
+      @auth.reset_with_role! :authorization
+      @auth.send(:approved_actions).should == []
+    end
+
+    it "should return false for #can_do_anything?" do
+      @auth.reset_with_role! :authorization
+      @auth.send(:can_do_anything?).should == false
+    end
+
+  end
+
 end
